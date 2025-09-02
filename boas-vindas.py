cd0f696f147d9b0314dca425e983833c72667209
joaobenittes <joaobenittes1970@gmail.com>
2025-09-02 20:44:58 -0300

    primeiro commit

---
+++ boas-vindas.py
@@ -0,0 +1,15 @@
+# coleta das variaveis
+nome = input ("informe o seu nome -> ")
+idade = int(input ("informe a sua idade -> "))
+
+# mensagem de boas vindas
+print("ola ", nome, "seja bem vindo !")
+idade_futura = idade + 5   
+
+# apresentar idade futura
+print("A sua idade futura 5 anos sera -> ", idade_futura)
+
+
+
+
+
---
+++ tarefas.py
@@ -0,0 +1,5 @@
+numero_1 = int(input("informe o numero -> "))
+print("o dobro ", numero_1 * 2) 
+lado = int(input("informe o lado do quadrado -> "))  
+print("a area e ", lado * lado)      
+

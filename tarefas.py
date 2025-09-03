8a32463d5c9799d7b892c3c8c4e60b72c49c9a36
joaobenittes <joaobenittes1970@gmail.com>
2025-09-02 21:51:21 -0300

    criacao do programa de tarefas

--- tarefas.py
+++ tarefas.py
@@ -1,5 +1,19 @@
-numero_1 = int(input("informe o numero -> "))
-print("o dobro ", numero_1 * 2) 
-lado = int(input("informe o lado do quadrado -> "))  
-print("a area e ", lado * lado)      
+# calculando o dobro do numero informado
 
+numero_1 = int(input("informe um numero inteiro -> "))
+print("o dobro e ", numero_1 * 2) 
+
+# calculando a area do quadrado
+lado = float(input("informe o valor do lado do quadrado  "))  
+print("a areado quadrado  e -> ", lado * lado)      
+
+# calcular a idade em meses e em dias
+
+idade = int(input("informe sua idade -> "))
+meses = idade * 12
+dias = meses * 365
+print("sua idade em meses e " , meses , "e em dias ", dias )
+    
+nota1 = float(input("informe a primeira nota -> "))
+nota2 = float(input("informe a segunda nota ->  "))
+nota3 =float(input("informe a terceira nota -> "))
